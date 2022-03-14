576c11ade61e8dd24e8648550cda0bb8f485fd9f
carpemonf <carpemonf@gmail.com>
2022-03-14 14:14:55 +0100

    Adding missing license notice

--- offline/OpenFOAM/incompressible/simpleFoam/pitzDaily/run_offline.sh
+++ offline/OpenFOAM/incompressible/simpleFoam/pitzDaily/run_offline.sh
@@ -1,4 +1,9 @@
 #/bin/bash
+#
+# Copyright (c) 2022 Carlos Peña-Monferrer. All rights reserved.
+# This work is licensed under the terms of the GNU LGPL v3.0 license.
+# For a copy, see <https://opensource.org/licenses/LGPL-3.0>.
+#
 
 run_case () {
   mkdir -p runs/$k/constant && \
